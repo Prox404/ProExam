506371eaadab89750105ecfcfd21ea7b9685b326
Công Luận <duongnguyencongluan@gmail.com>
2024-02-27 22:42:21 +0700

    Update page

--- Frontend/src/pages/ParseQuestions/ParseQuestions.jsx
+++ Frontend/src/pages/ParseQuestions/ParseQuestions.jsx
@@ -4,73 +4,55 @@ import {
   Button,
   Checkbox,
   IconButton,
-  Typography,
   TextField,
-  Select,
-  MenuItem,
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import DeleteIcon from "@mui/icons-material/Delete";
 import CloseIcon from "@mui/icons-material/Close";
-import { v4 as uuidv4 } from "uuid";
+
 
 function ParseQuestions() {
   const theme = useTheme();
+
   const [deleteEnabled, setDeleteEnabled] = useState(true);
 
   const [questions, setQuestions] = useState([
     {
-      id: uuidv4(),
       question: "",
-      answers: [
-        {
-          answer_text: "",
-          isCorrect: false,
-        },
-      ],
-      correctAnswers: [],
+      answers: Array.from({ length: 2 }, () => ({
+        answer_text: "",
+        isCorrect: false,
+      })),
     },
   ]);
 
-  const [questionTypes, setQuestionTypes] = useState([
-    { id: questions[0].id, type: "an_answer" },
-  ]);
-  const [answerCounts, setAnswerCounts] = useState(
-    Array(questions.length).fill(4)
-  );
+  const [answerCounts, setAnswerCounts] = useState(Array(questions.length).fill(4));
 
   const handleAddQuestion = () => {
     const newQuestion = {
-      id: uuidv4(),
       question: "",
-      answers: [
-        {
-          answer_text: "",
-          isCorrect: false,
-        },
-      ],
-      correctAnswers: [],
+      answers: Array.from({ length: 2 }, () => ({
+        answer_text: "",
+        isCorrect: false,
+      })),
     };
 
+    console.log("New Question:", newQuestion);
+
     setQuestions([...questions, newQuestion]);
-    setQuestionTypes([
-      ...questionTypes,
-      { id: newQuestion.id, type: "an_answer" },
-    ]);
     setAnswerCounts([...answerCounts, 4]);
-    //
-    const newAnswerCount = 4;
+    const newAnswerCount = 7;
     if (newAnswerCount > 2) {
       setDeleteEnabled(true);
     }
-    // console.log(newQuestion.id.uuidv4);
-    console.log(questions);
   };
 
   const handleAddAnswer = (questionIndex) => {
     const newQuestions = [...questions];
     const question = newQuestions[questionIndex];
-    question.answers.push("");
+    question.answers.push({ answer_text: "", isCorrect: false });
+
+    console.log("New Answer", newQuestions);
+
     setQuestions(newQuestions);
     const currentAnswerCount = question.answers.length;
     if (currentAnswerCount > 2) {
@@ -82,83 +64,74 @@ function ParseQuestions() {
     const newQuestions = [...questions];
     newQuestions[index].question = value;
     setQuestions(newQuestions);
-    console.log(newQuestions[index].question);
   };
 
   const handleAnswerChange = (questionIndex, answerIndex, value) => {
     const newQuestions = [...questions];
-    newQuestions[questionIndex].answers[answerIndex] = value;
+    newQuestions[questionIndex].answers[answerIndex].answer_text = value;
     setQuestions(newQuestions);
   };
 
-  const handleCorrectAnswerChange = (questionId, answerIndex) => {
-    setQuestions(
-      questions.map((question) => {
-        if (question.id === questionId) {
-          return {
+  // const handleCorrectAnswerChange = (questionIndex, answerIndex) => {
+  //   setQuestions(
+  //     questions.map((question, idx) => {
+  //       if (idx === questionIndex) {
+  //         return {
+  //           ...question,
+  //           answers: question.answers.map((answer, index) => {
+  //             if (index === answerIndex) {
+  //               return {
+  //                 ...answer,
+  //                 isCorrect: !answer.isCorrect,
+  //               };
+  //             }
+  //             return answer;
+  //           }),
+  //         };
+  //       }
+  //       return question;
+  //     })
+  //   );
+  // };
+  const handleCorrectAnswerChange = (questionIndex, answerIndex) => {
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = prevQuestions.map((question, idx) => {
+        if (idx === questionIndex) {
+          const updatedAnswers = question.answers.map((answer, index) => {
+            if (index === answerIndex) {
+              const updatedAnswer = {
+                ...answer,
+                isCorrect: !answer.isCorrect,
+              };
+
+              console.log(updatedAnswer); 
+
+              return updatedAnswer;
+            }
+            return answer;
+          });
+  
+          const updatedQuestion = {
             ...question,
-            correctAnswers:
-              questionTypes.find((item) => item.id === questionId).type ===
-              "many_answer"
-                ? toggleCorrectAnswer(question.correctAnswers, answerIndex)
-                : [answerIndex],
-            //
-            answers: question.answers.map((answer, index) => {
-              if (index === answerIndex) {
-                return {
-                  ...answer,
-                  isCorrect: !answer.isCorrect, // Đảo ngược trạng thái isCorrect
-                };
-              }
-              return answer;
-            }),
+            answers: updatedAnswers,
           };
+
+          console.log(updatedQuestion); 
+          
+          return updatedQuestion;
         }
-        console.log(answer.isCorrect);
-
         return question;
-      })
-    );
+      });
+  
+      return updatedQuestions;
+    });
   };
-
-  const toggleCorrectAnswer = (currentCorrectAnswers, answerIndex) => {
-    const index = currentCorrectAnswers.indexOf(answerIndex);
-    if (index === -1) {
-      return [...currentCorrectAnswers, answerIndex];
-    } else {
-      return currentCorrectAnswers.filter((idx) => idx !== answerIndex);
-    }
-  };
-
-  const handleDeleteQuestion = (id) => {
-    const indexToDelete = questions.findIndex((item) => item.id === id);
-    if (indexToDelete != -1) {
-      let newQuestions = [];
-      if (questions.length === 1) {
-        newQuestions = [
-          {
-            id: uuidv4(),
-            question: "",
-            answers: ["", "", "", ""],
-            correctAnswers: [],
-          },
-        ];
-      } else {
-        newQuestions = questions.filter((item) => {
-          return !(item.id === id);
-        });
-      }
-      setQuestions(newQuestions);
-    }
-  };
+  
 
   const handleDeleteAnswer = (questionIndex, answerIndex) => {
     const newQuestions = [...questions];
     const question = newQuestions[questionIndex];
     question.answers.splice(answerIndex, 1);
-    question.correctAnswers = question.correctAnswers.filter(
-      (idx) => idx !== answerIndex
-    );
     setQuestions(newQuestions);
     const currentAnswerCount = question.answers.length;
     if (currentAnswerCount <= 2) {
@@ -166,24 +139,6 @@ function ParseQuestions() {
     } else if (!deleteEnabled && currentAnswerCount > 2) {
       setDeleteEnabled(true);
     }
-    console.log(newQuestions);
-    console.log(answerIndex);
-  };
-
-  const handleTypeChange = (index, value) => {
-    const newQuestionTypes = [...questionTypes];
-    newQuestionTypes[index] = { id: questions[index].id, type: value };
-    setQuestionTypes(newQuestionTypes);
-    const newQuestions = questions.map((question, i) => {
-      if (i === index) {
-        return {
-          ...question,
-          correctAnswers: [],
-        };
-      }
-      return question;
-    });
-    setQuestions(newQuestions);
   };
 
   return (
@@ -211,66 +166,19 @@ function ParseQuestions() {
             alignItems: "flex-start",
             padding: "10px",
             height: "auto",
-            // overflow: "auto",
           }}
         >
-          <IconButton
-            sx={{
-              color: theme.palette.trashColor,
-              position: "absolute",
-              top: "10px",
-              right: "10px",
-              "&:hover": {
-                color: "#435ebe",
-              },
-            }}
-            onClick={() => handleDeleteQuestion(question.id)}
-          >
-            <DeleteIcon />
-          </IconButton>
-
-          <Typography
-            variant="h5"
-            sx={{
-              color: theme.palette.textBlack,
-              marginBottom: "10px",
-              fontWeight: "700",
-            }}
-          >
-            Question {index + 1}:
-          </Typography>
-
           <Box sx={{ display: "flex", alignItems: "center" }}>
             <TextField
-              label="Question..."
+              label={`Question ${index + 1}:`}
               variant="outlined"
               fullWidth
               value={question.question}
               onChange={(e) => handleQuestionChange(index, e.target.value)}
               sx={{
-                width: {
-                  xs: "70%",
-                  sm: "85%",
-                },
                 marginBottom: "10px",
               }}
             />
-            <Select
-              value={questionTypes[index].type || ""}
-              onChange={(e) => handleTypeChange(index, e.target.value)}
-              variant="outlined"
-              sx={{
-                width: {
-                  xs: "30%",
-                  sm: "15%",
-                },
-                marginLeft: "10px",
-                marginBottom: "10px",
-              }}
-            >
-              <MenuItem value="an_answer">An answer</MenuItem>
-              <MenuItem value="many_answer">Many answer</MenuItem>
-            </Select>
           </Box>
 
           {question.answers.map((answer, answerIndex) => (
@@ -300,21 +208,16 @@ function ParseQuestions() {
               </IconButton>
               <Checkbox
                 color="primary"
-                checked={
-                  questionTypes[index].type === "an_answer"
-                    ? question.correctAnswers.includes(answerIndex)
-                    : question.correctAnswers.includes(answerIndex)
-                }
-                onChange={() =>
-                  handleCorrectAnswerChange(question.id, answerIndex)
-                }
+                checked={answer.isCorrect}
+                onChange={() => handleCorrectAnswerChange(index, answerIndex)}
               />
 
               <TextField
+                label={`Answer ${answerIndex + 1}`}
                 variant="outlined"
                 fullWidth
                 size="small"
-                value={answer}
+                value={answer.answer_text}
                 onChange={(e) =>
                   handleAnswerChange(index, answerIndex, e.target.value)
                 }
@@ -326,6 +229,7 @@ function ParseQuestions() {
             variant="outlined"
             onClick={() => handleAddAnswer(index)}
             sx={{
+              color: theme.palette.textColorSecondary,
               textTransform: "none",
             }}
           >
